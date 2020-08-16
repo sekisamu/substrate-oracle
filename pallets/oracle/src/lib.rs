fe6fb14db7878082c19135a3aff2a2ed3d58731c
hammeWang <dsw0602@foxmail.com>
2020-08-17 01:42:01 +0800

    add fetch_Data

--- pallets/oracle/src/lib.rs
+++ pallets/oracle/src/lib.rs
@@ -1,111 +1,91 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+// TODO: we need to figure out the key function here
+// which is how to convert JsonValue::number to PrimitiveOracleType
+
 use frame_support::{
-    decl_error, decl_event, decl_module, decl_storage, dispatch, traits::Get,
-    IterableStorageDoubleMap, IterableStorageMap,
+	debug,
+	decl_error, decl_event, decl_module, decl_storage, dispatch, traits::Get,
+	IterableStorageDoubleMap, IterableStorageMap,
 };
-use frame_system::{ensure_root, ensure_signed};
+use frame_system::{ensure_root, ensure_signed, offchain::{
+	AppCrypto, CreateSignedTransaction, SendUnsignedTransaction, SendSignedTransaction,
+	SignedPayload, SigningTypes, Signer, SubmitTransaction,
+	},
+};
+use sp_runtime::{
+	offchain::{http, Duration, storage::StorageValueRef},
+	transaction_validity::{
+		InvalidTransaction, ValidTransaction, TransactionValidity, TransactionSource,
+		TransactionPriority,
+	},
+};
 
 use codec::{Decode, Encode};
 use sp_runtime::{
-    curve::PiecewiseLinear,
-    traits::{
-        AtLeast32BitUnsigned, CheckedSub, Convert, Dispatchable, SaturatedConversion, Saturating,
-        StaticLookup, Zero,
-    },
-    DispatchError, DispatchResult, PerThing, PerU16, Percent, Permill, RuntimeDebug,
+	curve::PiecewiseLinear,
+	traits::{
+		AtLeast32BitUnsigned, CheckedSub, Convert, Dispatchable, SaturatedConversion, Saturating,
+		StaticLookup, Zero,
+	},
+	DispatchError, DispatchResult, PerThing, PerU16, Percent, Permill, RuntimeDebug,
 };
-use sp_std::prelude::*;
+use sp_std::{prelude::*, str::Chars};
+use lite_json::json::JsonValue;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Encode, Decode, RuntimeDebug)]
 pub enum Operations {
-    Average,
-    Sum,
+	Average,
+	Sum,
 }
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Encode, Decode, RuntimeDebug)]
 pub enum PrimitiveOracleType {
-    U8(u8),
-    U16(u16),
-    U32(u32),
-    U64(u64),
-    U128(u128),
-    I8(i8),
-    I16(i16),
-    I32(i32),
-    I64(i64),
-    I128(i128),
-    Percent(Percent),
-    Permill(Permill),
+	U8(u8),
+	U16(u16),
+	U32(u32),
+	Percent(Percent),
+	Permill(Permill),
 }
 
 impl PrimitiveOracleType {
-    pub fn number_type(&self) -> NumberType {
-        match self {
-            PrimitiveOracleType::U8(_) => NumberType::U8,
-            PrimitiveOracleType::U16(_) => NumberType::U16,
-            PrimitiveOracleType::U32(_) => NumberType::U32,
-            PrimitiveOracleType::U64(_) => NumberType::U64,
-            PrimitiveOracleType::U128(_) => NumberType::U128,
-            PrimitiveOracleType::I8(_) => NumberType::I8,
-            PrimitiveOracleType::I16(_) => NumberType::I16,
-            PrimitiveOracleType::I32(_) => NumberType::I32,
-            PrimitiveOracleType::I64(_) => NumberType::I64,
-            PrimitiveOracleType::I128(_) => NumberType::I128,
-            PrimitiveOracleType::Percent(_) => NumberType::Percent,
-            PrimitiveOracleType::Permill(_) => NumberType::Permill,
-        }
-    }
+	pub fn number_type(&self) -> NumberType {
+		match self {
+			PrimitiveOracleType::U8(_) => NumberType::U8,
+			PrimitiveOracleType::U16(_) => NumberType::U16,
+			PrimitiveOracleType::U32(_) => NumberType::U32,
+			PrimitiveOracleType::Percent(_) => NumberType::Percent,
+			PrimitiveOracleType::Permill(_) => NumberType::Permill,
+		}
+	}
 }
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Encode, Decode, RuntimeDebug)]
 pub enum NumberType {
-    U8,
-    U16,
-    U32,
-    U64,
-    U128,
-    I8,
-    I16,
-    I32,
-    I64,
-    I128,
-    Percent,
-    Permill,
+	U8,
+	U16,
+	U32,
+	U64,
+	Percent,
+	Permill,
 }
-/*
-impl NumberType {
-    pub fn init_primitive_type(&self) -> PrimitiveOracleType {
-        match self {
-            NumberType::U8 => PrimitiveOracleType::U8(Default::default()),
-            NumberType::U16 => PrimitiveOracleType::U16(Default::default()),
-            NumberType::U32 => PrimitiveOracleType::U32(Default::default()),
-            NumberType::U64 => PrimitiveOracleType::U64(Default::default()),
-            NumberType::U128 => PrimitiveOracleType::U128(Default::default()),
-            NumberType::I8 => PrimitiveOracleType::I8(Default::default()),
-            NumberType::I16 => PrimitiveOracleType::I16(Default::default()),
-            NumberType::I32 => PrimitiveOracleType::I32(Default::default()),
-            NumberType::I64 => PrimitiveOracleType::I64(Default::default()),
-            NumberType::I128 => PrimitiveOracleType::I128(Default::default()),
-            NumberType::Percent => PrimitiveOracleType::Percent(Default::default()),
-            NumberType::Permill => PrimitiveOracleType::Permill(Default::default()),
-        }
-    }
-}*/
+
 
 impl Default for PrimitiveOracleType {
-    fn default() -> Self {
-        PrimitiveOracleType::U8(0)
-    }
+	fn default() -> Self {
+		PrimitiveOracleType::U8(0)
+	}
 }
 
 type StorageKey = Vec<u8>;
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Encode, Decode, RuntimeDebug)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, RuntimeDebug)]
 pub struct Info<BlockNumber> {
-    number_type: NumberType,
-    operation: Operations,
-    schedule: BlockNumber,
+	/// the key of what we want from the json callback
+	pub key_str: Vec<u8>,
+	number_type: NumberType,
+	operation: Operations,
+	schedule: BlockNumber,
 }
 
 const RING_BUF_LEN: usize = 8;
@@ -114,23 +94,24 @@ use frame_support::traits::{Contains, EnsureOrigin};
 use sp_std::{marker::PhantomData, prelude::*};
 
 pub struct DataFeedGet<Key: Get<[u8; 32]>, Value: Get<PrimitiveOracleType>>(
-    PhantomData<Key>,
-    PhantomData<Value>,
+	PhantomData<Key>,
+	PhantomData<Value>,
 );
+
 impl<Key: Get<[u8; 32]>, Value: Get<PrimitiveOracleType>> Get<PrimitiveOracleType>
-    for DataFeedGet<Key, Value>
+for DataFeedGet<Key, Value>
 {
-    fn get() -> PrimitiveOracleType {
-        frame_support::storage::unhashed::get_or(Key::get().as_ref(), Value::get())
-    }
+	fn get() -> PrimitiveOracleType {
+		frame_support::storage::unhashed::get_or(Key::get().as_ref(), Value::get())
+	}
 }
 
 pub trait Trait: frame_system::Trait {
-    /// Because this pallet emits events, it depends on the runtime's definition of an event.
-    type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+	/// Because this pallet emits events, it depends on the runtime's definition of an event.
+	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 
-    /// The origin that can schedule an update
-    type DispatchOrigin: EnsureOrigin<Self::Origin, Success = Self::AccountId>;
+	/// The origin that can schedule an update
+	type DispatchOrigin: EnsureOrigin<Self::Origin, Success=Self::AccountId>;
 }
 
 decl_event!(
@@ -164,15 +145,20 @@ decl_storage! {
 
         pub Infos get(fn infos): map hasher(twox_64_concat) StorageKey => Option<Info<T::BlockNumber>>;
 
+        /// permissioned URL that could be used to fetch data
+        pub Url get(fn url): map hasher(twox_64_concat) StorageKey => Vec<u8>;
+
         pub DataFeeds get(fn member_score): double_map hasher(blake2_128_concat) StorageKey,
             hasher(blake2_128_concat) T::AccountId => Option<[PrimitiveOracleType; RING_BUF_LEN]>;
+
+
     }
 }
 
 impl<T: Trait> Contains<T::AccountId> for Module<T> {
-    fn sorted_members() -> Vec<T::AccountId> {
-        Self::all_providers()
-    }
+	fn sorted_members() -> Vec<T::AccountId> {
+		Self::all_providers()
+	}
 }
 
 decl_module! {
@@ -247,58 +233,130 @@ decl_module! {
     }
 }
 impl<T: Trait> Module<T> {
-    fn feed_data_impl(
-        who: T::AccountId,
-        key: StorageKey,
-        value: PrimitiveOracleType,
-    ) -> DispatchResult {
-        if !Self::all_types().contains(&key) {
-            Err(Error::<T>::InvalidKey)?
-        }
-        let info: Info<_> = Self::infos(&key).ok_or(Error::<T>::InvalidKey)?;
-        if value.number_type() != info.number_type {
-            Err(Error::<T>::InvalidValue)?
-        }
-        DataFeeds::<T>::try_mutate_exists(key, who, |data| {
-            match data {
-                Some(data) => {
-                    let mut new_data = [Default::default(); RING_BUF_LEN];
-                    new_data[0] = value;
-                    // move data buf to new_data buf, and drop last item
-                    new_data[1..].copy_from_slice(&data[0..(data.len() - 2)]);
-                    *data = new_data;
-                }
-                None => {
-                    *data = Some([value; RING_BUF_LEN]);
-                }
-            }
-            Ok(())
-        })
-    }
+	fn feed_data_impl(
+		who: T::AccountId,
+		key: StorageKey,
+		value: PrimitiveOracleType,
+	) -> DispatchResult {
+		if !Self::all_types().contains(&key) {
+			Err(Error::<T>::InvalidKey)?
+		}
+		let info: Info<_> = Self::infos(&key).ok_or(Error::<T>::InvalidKey)?;
+		if value.number_type() != info.number_type {
+			Err(Error::<T>::InvalidValue)?
+		}
+		DataFeeds::<T>::try_mutate_exists(key, who, |data| {
+			match data {
+				Some(data) => {
+					let mut new_data = [Default::default(); RING_BUF_LEN];
+					new_data[0] = value;
+					// move data buf to new_data buf, and drop last item
+					new_data[1..].copy_from_slice(&data[0..(data.len() - 2)]);
+					*data = new_data;
+				}
+				None => {
+					*data = Some([value; RING_BUF_LEN]);
+				}
+			}
+			Ok(())
+		})
+	}
 
-    fn calc(key: StorageKey, info: Info<T::BlockNumber>) {
-        for (account_id, datas) in DataFeeds::<T>::drain_prefix(&key) {
-            match info.operation {
-                Operations::Sum => {
-                    // TODO
-                    PrimitiveOracleType::default()
-                }
-                Operations::Average => {
-                    // TODO
-                    PrimitiveOracleType::default()
-                }
-            };
-        }
-        // TODO
-        let value = PrimitiveOracleType::default();
-        Self::set_storage_value(key, value);
-    }
+	fn calc(key: StorageKey, info: Info<T::BlockNumber>) {
+		for (account_id, datas) in DataFeeds::<T>::drain_prefix(&key) {
+			match info.operation {
+				Operations::Sum => {
+					// TODO
+					PrimitiveOracleType::default()
+				}
+				Operations::Average => {
+					// TODO
+					PrimitiveOracleType::default()
+				}
+			};
+		}
+		// TODO
+		let value = PrimitiveOracleType::default();
+		Self::set_storage_value(key, value);
+	}
 
-    fn set_storage_value(key: Vec<u8>, value: PrimitiveOracleType) {
-        frame_support::storage::unhashed::put(&key, &value);
-    }
+	fn set_storage_value(key: Vec<u8>, value: PrimitiveOracleType) {
+		frame_support::storage::unhashed::put(&key, &value);
+	}
+
+	fn fetch_data(storage_key: &StorageKey) -> Result<PrimitiveOracleType, http::Error> {
+		// We want to keep the offchain worker execution time reasonable, so we set a hard-coded
+		// deadline to 2s to complete the external call.
+		// You can also wait idefinitely for the response, however you may still get a timeout
+		// coming from the host machine.
+		let deadline = sp_io::offchain::timestamp().add(Duration::from_millis(2_000));
+		// TODO: if error
+		let url = Self::url(storage_key);
+
+		let request = http::Request::get(
+			// TODO: if error
+			core::str::from_utf8(&url).unwrap());
+
+		// to alter request headers or stream body content in case of non-GET requests.
+		let pending = request
+			.deadline(deadline)
+			.send()
+			.map_err(|_| http::Error::IoError)?;
+
+
+		let response = pending.try_wait(deadline)
+			.map_err(|_| http::Error::DeadlineReached)??;
+		// Let's check the status code before we proceed to reading the response.
+		if response.code != 200 {
+		debug::warn!("Unexpected status code: {}", response.code);
+		return Err(http::Error::Unknown);
+		}
+
+
+		let body = response.body().collect::<Vec<u8>>();
+		// TODO: handle error here
+		let info = Self::infos(&storage_key).unwrap();
+
+		// Create a str slice from the body.
+		let body_str = sp_std::str::from_utf8(&body).map_err(|_| {
+			debug::warn!("No UTF8 body");
+			http::Error::Unknown
+		})?;
+		// TODO: handle error
+		let key_str = core::str::from_utf8(&info.key_str).unwrap().chars();
+		let data = match Self::parse_data(key_str, body_str) {
+			Some(data) => Ok(data),
+			None => {
+				debug::warn!("Unable to extract price from the response: {:?}", body_str);
+				Err(http::Error::Unknown)
+			}
+		}?;
+
+//		debug::warn!("Got Data: {}", data);
+
+		Ok(data)
+	}
+
+	fn parse_data(key_str: Chars, data: &str) -> Option<PrimitiveOracleType> {
+		let mut key_str = key_str;
+		let val = lite_json::parse_json(data);
+		let output = val.ok().and_then(|v| match v {
+			JsonValue::Object(obj) => {
+				obj.into_iter().find(
+					|(k, _)| k.iter().all(|k| Some(*k) == key_str.next()))
+						.and_then(|v| match v.1 {
+							JsonValue::Number(number) => Some(number),
+							_ => None,
+						})
+			},
+			_ => None,
+		})?;
+
+		// TODO: transform number into PrimitiveOracleType
+		Some(PrimitiveOracleType::default())
+	}
+
+
 }
 
-// fn calc(src: &[PrimitiveOracleType], expect_type: NumberType, op: Operations) -> PrimitiveOracleType {
-//     src.iter().filter(|item| *item.number_type() == expect_type)
-// }
+
